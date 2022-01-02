a3b60a27e0bf4b15653081868ab708dfd7342fdd
Ken Chan <chankamlam@icloud.com>
2022-01-02 08:21:21 +0800

    update

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 ```ascii
-                       _               __                     __
-                      (_)_____        / /_ ____ ___   ____   / /
-                     / // ___/______ / __// __ `__ \ / __ \ / / 
-                    / /(__  )/_____// /_ / / / / / // /_/ // /  
-                 __/ //____/        \__//_/ /_/ /_// .___//_/   
-                /___/                             /_/           
+                      _               __                     __                            _            
+                     (_)_____        / /_ ____ ___   ____   / /       ___   ____   ____ _ (_)____   ___ 
+                    / // ___/______ / __// __ `__ \ / __ \ / /______ / _ \ / __ \ / __ `// // __ \ / _ \
+                   / /(__  )/_____// /_ / / / / / // /_/ // //_____//  __// / / // /_/ // // / / //  __/
+                __/ //____/        \__//_/ /_/ /_// .___//_/        \___//_/ /_/ \__, //_//_/ /_/ \___/ 
+               /___/                             /_/                            /____/                  
+
 ```
 [![Node.js CI](https://github.com/chankamlam/js-tmpl/actions/workflows/node.js.yml/badge.svg)](https://github.com/chankamlam/js-tmpl/actions/workflows/node.js.yml)
 [![MIT Licence](https://badges.frapsoft.com/os/mit/mit.svg?v=103)](https://opensource.org/licenses/mit-license.php)
--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,14 @@
 const { expect } = require("@jest/globals");
-const scissor = require("../lib/index")
-describe("Test scissor", () => {
-    test("scissor init normaly", () => {
-        expect(scissor).toHaveProperty("config")
-        expect(scissor).toHaveProperty("render")
+const te = require("../lib/index")
+describe("Test template engine", () => {
+    test("template engine init normaly", () => {
+        expect(te).toHaveProperty("config")
+        expect(te).toHaveProperty("render")
     });
-    test("scissor config normaly", () => {
-        expect(scissor.config({})).toBeTruthy
+    test("template engine config normaly", () => {
+        expect(te.config({})).toBeTruthy
     })
-    test("scissor render normaly", () => {
+    test("template engine render normaly", () => {
         var tmpl = `<div>
 My name is <%=name%> and i am <%=age%> years old.
 I live in <%=contact.address%> and <%my phone number is <%=contact.phone%>.
@@ -28,7 +28,7 @@ My favorite coins are:
             },
             coins: ["ETH", "BTC"]
         }
-        const res = scissor.render(tmpl,data)
+        const res = te.render(tmpl,data)
         expect(res).toEqual("<div> My name is ckl and i am 18 years old. I live in xxxxxxxx and <%my phone number is 888. My favorite coins are: <ul>      <li>ETH</li>      <li>BTC</li>  </ul> </div>")
     })
 });
